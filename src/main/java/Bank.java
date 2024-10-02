8384603bf1e6153afe119292818db32d320baa56
LianneCampbell <lianne.campbell@keyin.com>
2024-10-02 09:38:23 -0230

    Setup maven project

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,2 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bank {
+    private List<Account> accounts;
+
+    public Bank() {
+        accounts = new ArrayList<>();
+    }
+
+    public void createAccount(Account account) {
+        accounts.add(account);
+        System.out.println("Account created: " + account.getAccountNumber());
+    }
+
+    public Account findAccount(String accountNumber) {
+        for (Account account : accounts) {
+            if (account.getAccountNumber().equals(accountNumber)) {
+                return account;
+            }
+        }
+        System.out.println("Account not found.");
+        return null;
+    }
+
+    public void removeAccount(String accountNumber) {
+        Account accountToRemove = findAccount(accountNumber);
+        if (accountToRemove != null) {
+            accounts.remove(accountToRemove);
+            System.out.println("Account removed: " + accountNumber);
+        }
+    }
 }
